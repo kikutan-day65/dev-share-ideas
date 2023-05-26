e3b4b508fc63fce29d17bed7aa276c590228e6c0
Haruto Mori <baystarsman@outlook.jp>
2023-05-26 21:27:51 +0900

    feat: change the name of function and its url

--- ShareIdea/projects/urls.py
+++ ShareIdea/projects/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.projects, name='projects'),
+    path('', views.ideas, name='ideas'),
+    path('idea-detail/<str:pk>/', views.idea_detail, name='idea-detail'),
 ]
--- ShareIdea/projects/views.py
+++ ShareIdea/projects/views.py
@@ -2,11 +2,21 @@ from django.shortcuts import render, redirect
 
 from .models import Idea
 
-def projects(request):
+def ideas(request):
     ideas = Idea.objects.all()    
 
     context = {
         'ideas': ideas,
     }
 
-    return render(request, 'projects/projects.html', context)
+    return render(request, 'projects/ideas.html', context)
+
+
+def idea_detail(request, pk):
+    detail = Idea.objects.get(id=pk)
+    
+    context = {
+        'detail': detail,
+    }
+
+    return render(request, 'projects/idea_detail.html', context)
