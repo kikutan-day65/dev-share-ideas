9a03ae17c7b28cbc2871cebace9f07bb8715ad2a
Haruto Mori <baystarsman@outlook.jp>
2023-05-26 21:24:00 +0900

    feat: get ideas data

--- ShareIdea/projects/views.py
+++ ShareIdea/projects/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 
+from .models import Idea
 
 def projects(request):
+    ideas = Idea.objects.all()    
 
     context = {
-
+        'ideas': ideas,
     }
 
     return render(request, 'projects/projects.html', context)
