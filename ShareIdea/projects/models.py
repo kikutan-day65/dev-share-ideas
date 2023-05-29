28b45a6289f3bbac4de124e9263a86fba350f287
Haruto Mori <baystarsman@outlook.jp>
2023-05-29 12:17:57 +0900

    feat: change the tags model to tag model and add many-to-many relationship

--- ShareIdea/projects/models.py
+++ ShareIdea/projects/models.py
@@ -9,7 +9,7 @@ class Idea(models.Model):
     idea_link = models.CharField(max_length=2000, null=True, blank=True)
     view_total = models.IntegerField(default=0, null=True, blank=True)
     like = models.IntegerField(default=0, null=True, blank=True)
-    # tags =
+    tags = models.ManyToManyField('Tag', blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(
         default=uuid.uuid4,
@@ -22,9 +22,15 @@ class Idea(models.Model):
         return self.title
     
 
-class Tags(models.Model):
+class Tag(models.Model):
     name = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4,
+        unique=True,
+        primary_key=True,
+        editable=False
+    )
 
     def __str__(self):
         return self.name
