8df6bc88ffc1a21e403158114c5f09bba62124bc
Haruto Mori <baystarsman@outlook.jp>
2023-05-30 12:43:04 +0900

    feat: create a skill model

--- ShareIdea/users/models.py
+++ ShareIdea/users/models.py
@@ -30,3 +30,19 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.name)
+    
+
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4,
+        unique=True,
+        primary_key=True,
+        editable=False
+    )
+
+    def __str__(self):
+        return str(self.name)
