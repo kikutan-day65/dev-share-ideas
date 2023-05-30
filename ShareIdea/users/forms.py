84aa4cfc2d00278419a91385cf71320e0427ea5f
Haruto Mori <baystarsman@outlook.jp>
2023-05-30 11:27:56 +0900

    feat: create a profile form

--- ShareIdea/users/forms.py
+++ ShareIdea/users/forms.py
@@ -2,6 +2,8 @@ from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+from .models import Profile
+
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
@@ -17,3 +19,14 @@ class CustomUserCreationForm(UserCreationForm):
         labels = {
             'first_name': 'Name',
         }
+
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = [
+            'name', 'username', 'email',
+            'headline', 'bio', 'location',
+            'user_image', 'git_link', 'linkedin_link',
+            'twitter_link'
+        ]
