644d386298e9a425a2159cbfbca59d8fc61a89aa
Haruto Mori <baystarsman@outlook.jp>
2023-06-02 23:26:48 +0900

    feat: add review model in admin page

--- ShareIdea/projects/admin.py
+++ ShareIdea/projects/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Idea, Tag
+from .models import Idea, Tag, Review
 
 admin.site.register(Idea)
 admin.site.register(Tag)
+admin.site.register(Review)
