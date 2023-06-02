4d6561d062fd82981942dc6429e488fc9e4698e2
Haruto Mori <baystarsman@outlook.jp>
2023-06-02 23:26:16 +0900

    feat: create review model

--- ShareIdea/projects/models.py
+++ ShareIdea/projects/models.py
@@ -12,6 +12,7 @@ class Idea(models.Model):
     idea_link = models.CharField(max_length=2000, null=True, blank=True)
     view_total = models.IntegerField(default=0, null=True, blank=True)
     like = models.IntegerField(default=0, null=True, blank=True)
+    dislike = models.IntegerField(default=0, null=True, blank=True)
     tags = models.ManyToManyField('Tag', blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(
@@ -21,11 +22,53 @@ class Idea(models.Model):
         editable=False
     )
 
+    @property
+    def reviewers(self):
+        queryset = self.review_set.all().values_list('owner_id', flat=True)
+        return queryset
+    
+    @property
+    def get_vote_count(self):
+        reviews = self.review_set.all()
+        like_votes = reviews.filter(value='like').count()
+        dislike_votes = reviews.filter(value='dislike').count()
+
+        self.like = like_votes
+        self.dislike = dislike_votes
+
+        self.save()
+
     def __str__(self):
         return self.title
     
     class Meta:
-        ordering = ['-created']
+        ordering = ['-like', 'title', '-created']
+
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('like', 'like'),
+        ('dislike', 'dislike')
+    )
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    idea = models.ForeignKey(Idea, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4,
+        unique=True,
+        primary_key=True,
+        editable=False
+    )
+
+    class Meta:
+        unique_together = [
+            ['owner', 'idea']
+        ]
+    
+    def __str__(self):
+        return self.value
     
 
 class Tag(models.Model):
