8be33d8a308130329f0682ae68f1f9c069af8377
Haruto Mori <baystarsman@outlook.jp>
2023-06-03 13:39:58 +0900

    feat: create a paginate users function

--- ShareIdea/users/utils.py
+++ ShareIdea/users/utils.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 from .models import Profile, Skill
 
@@ -18,5 +19,34 @@ def search_profiles(request):
         Q(skill__in=skills)
     )
 
-
     return profiles, search_query
+
+
+def paginate_users(request, users, results):
+
+    page = request.GET.get('page')
+    paginator = Paginator(users, results)
+
+    try:
+        users = paginator.page(page)
+    except PageNotAnInteger:
+        page = 1
+        users = paginator.page(page)
+    except EmptyPage:
+        page = paginator.num_pages
+        users = paginator.page(page)
+
+    left_index = (int(page) - 4)
+
+    if left_index < 1:
+        left_index = 1
+    
+    right_index = (int(page) + 5)
+
+    if right_index > paginator.num_pages:
+        right_index = paginator.num_pages + 1
+
+    custom_range = range(left_index, right_index)
+
+
+    return custom_range, users
