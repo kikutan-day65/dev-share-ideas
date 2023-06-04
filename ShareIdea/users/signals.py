12f4c99bbecc3dccae50b67d785ce865e908a86e
Haruto Mori <baystarsman@outlook.jp>
2023-06-04 19:09:19 +0900

    feat: add sendig email function

--- ShareIdea/users/signals.py
+++ ShareIdea/users/signals.py
@@ -1,6 +1,9 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
+from django.core.mail import send_mail
+from django.conf import settings
+
 from django.contrib.auth.models import User
 from .models import Profile
 
@@ -13,6 +16,17 @@ def create_profile(sender, instance, created, **kwargs):
             username=user.username,
             email=user.email,
             name=user.first_name,
+        )
+
+        subject = 'Welcome to Share Idea'
+        message = "We are glad that you join us!"
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False
         )
 
 
