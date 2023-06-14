f6cb6706fb24d6e37f968785f4e2be9eb61d1248
Haruto Mori <baystarsman@outlook.jp>
2023-06-14 09:04:14 +0900

    fix: fix the init file for message form

--- ShareIdea/users/forms.py
+++ ShareIdea/users/forms.py
@@ -70,7 +70,7 @@ class MessageForm(ModelForm):
         ]
     
     def __init__(self, *args, **kwargs):
-        super(ProfileForm, self).__init__(*args, **kwargs)
+        super(MessageForm, self).__init__(*args, **kwargs)
 
         for name, field in self.fields.items():
             field.widget.attrs.update({'class': 'form-control mb-3'})
