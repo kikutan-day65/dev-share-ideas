2186400c65c4fab4a503c43f0c29cae3ebefab74
Haruto Mori <baystarsman@outlook.jp>
2023-06-14 16:28:22 +0900

    feat: delete idea if its owner is deleted

--- ShareIdea/projects/models.py
+++ ShareIdea/projects/models.py
@@ -5,7 +5,7 @@ from users.models import Profile
 import uuid
 
 class Idea(models.Model):
-    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     image = models.ImageField(null=True, blank=True, default="idea-default.jpg")
